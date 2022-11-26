b4d1431d852c9112e8b4adb17ef3d70003682ef1
AnasFajoui <anasfajoui@gmail.com>
2022-11-26 22:34:06 +0100

    VolGenericPKid

--- src/main/java/ma/ac/emi/ginfo/hg/emiflights/repositories/VolGRepository.java
+++ src/main/java/ma/ac/emi/ginfo/hg/emiflights/repositories/VolGRepository.java
@@ -1,7 +1,8 @@
 package ma.ac.emi.ginfo.hg.emiflights.repositories;
 
-import ma.ac.emi.ginfo.hg.emiflights.entities.VolG;
+import ma.ac.emi.ginfo.hg.emiflights.entities.VolGeneric;
+import ma.ac.emi.ginfo.hg.emiflights.entities.VolGenericPKid;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VolGRepository extends JpaRepository<VolG, String> {
+public interface VolGRepository extends JpaRepository<VolGeneric, VolGenericPKid> {
 }

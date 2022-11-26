fdf535f687a87a8d432b90f45a0e78c7394512de
AnasFajoui <anasfajoui@gmail.com>
2022-11-26 14:03:37 +0100

    Initial commit

---
+++ src/main/java/ma/ac/emi/ginfo/hg/emiflights/EmiFlightsApplication.java
@@ -0,0 +1,13 @@
+package ma.ac.emi.ginfo.hg.emiflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmiFlightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmiFlightsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/ac/emi/ginfo/hg/emiflights/EmiFlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ac.emi.ginfo.hg.emiflights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmiFlightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

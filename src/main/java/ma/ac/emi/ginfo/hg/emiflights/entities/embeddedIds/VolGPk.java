0dfac9ec2a4962cd623a4a7bdb469aaf2dd78dfb
mannofsky <93436243+MannOfSky@users.noreply.github.com>
2022-11-26 22:48:15 +0100

    habrisha 3

---
+++ src/main/java/ma/ac/emi/ginfo/hg/emiflights/entities/embeddedIds/VolGPk.java
@@ -0,0 +1,9 @@
+package ma.ac.emi.ginfo.hg.emiflights.entities.embeddedIds;
+
+
+import java.io.Serializable;
+
+public class VolGPk implements Serializable {
+    private String jour;
+    private
+}
